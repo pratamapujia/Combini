d82f62af4b559435267058c86efda672074ad1fc
Pratama Puji <pujiistiani123@gmail.com>
2021-09-02 15:21:25 +0700

    update beranda 2-09-21

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+// $(document).ready(function () {
+//   $(window).scroll(function () {
+//     // scroll-up button show/hide script
+//     if (this.scrollY > 500) {
+//       $(".scroll-up-btn").addClass("show");
+//     } else {
+//       $(".scroll-up-btn").removeClass("show");
+//     }
+//   });
+//   // script slide up
+//   $(".scroll-up-btn").click(function () {
+//     $("html").animate({ scrollTop: 0 });
+//     // menghapus button scroll up
+//     $("html").css("scrollBehavior", "auto");
+//   });
+// });
