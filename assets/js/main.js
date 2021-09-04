8b198a8ca90729fd74736c9401833a207900f969
Pratama Puji <pujiistiani123@gmail.com>
2021-09-04 19:01:03 +0700

    update beranda 4-09-21

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,67 @@
-// $(document).ready(function () {
-//   $(window).scroll(function () {
-//     // scroll-up button show/hide script
-//     if (this.scrollY > 500) {
-//       $(".scroll-up-btn").addClass("show");
-//     } else {
-//       $(".scroll-up-btn").removeClass("show");
-//     }
-//   });
-//   // script slide up
-//   $(".scroll-up-btn").click(function () {
-//     $("html").animate({ scrollTop: 0 });
-//     // menghapus button scroll up
-//     $("html").css("scrollBehavior", "auto");
-//   });
-// });
+$(document).ready(function () {
+  $(window).scroll(function () {
+    // scroll-up button show/hide script
+    if (this.scrollY > 500) {
+      $(".scroll-up-btn").addClass("show");
+    } else {
+      $(".scroll-up-btn").removeClass("show");
+    }
+  });
+  // script slide up
+  $(".scroll-up-btn").click(function () {
+    $("html").animate({ scrollTop: 0 });
+    // menghapus button scroll up
+    $("html").css("scrollBehavior", "auto");
+  });
+
+  // Script Carousel
+  $(".carousel").owlCarousel({
+    margin: 20,
+    center: true,
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+      },
+      400: {
+        items: 2,
+        nav: false,
+      },
+      600: {
+        items: 3,
+        nav: false,
+      },
+      1000: {
+        items: 5,
+        nav: false,
+      },
+    },
+  });
+  $(".carouselTestimoni").owlCarousel({
+    margin: 20,
+    center: true,
+    margin: 20,
+    loop: true,
+    autoplay: true,
+    autoplayTimeOut: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+      },
+      600: {
+        items: 2,
+        nav: false,
+      },
+      1000: {
+        items: 3,
+        nav: false,
+      },
+    },
+  });
+});
