422d486a84993dc814393729b2219c6b83773b20
Pratama Puji <pujiistiani123@gmail.com>
2021-09-07 14:34:12 +0700

    update Slick Slider Beranda 7-09-21

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,6 +41,7 @@ $(document).ready(function () {
       },
     },
   });
+
   $(".carouselTestimoni").owlCarousel({
     margin: 20,
     center: true,
@@ -62,6 +63,41 @@ $(document).ready(function () {
         nav: false,
       },
     },
+  });
+
+  $(".topWeek").slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   });
 });
 
