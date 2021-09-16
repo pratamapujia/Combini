99028e8a2c518709869b5264a7bfcc24cd12da5d
Pratama Puji <pujiistiani123@gmail.com>
2021-09-17 04:35:43 +0700

    Update form login

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
   $(window).scroll(function () {
     // scroll-up button show/hide script
-    if (this.scrollY > 500) {
+    if (this.scrollY > 100) {
       $(".scroll-up-btn").addClass("show");
     } else {
       $(".scroll-up-btn").removeClass("show");
