9b09fc49814394a55812c5b985d27bae33766f49
Pratama Puji <pujiistiani123@gmail.com>
2021-09-20 16:28:52 +0700

    add view user 20-09-21

--- assets/js/main.js
+++ assets/js/main.js
@@ -50,33 +50,6 @@ $(document).ready(function () {
     ],
   });
 
-  // $(".carousel").owlCarousel({
-  //   margin: 20,
-  //   center: true,
-  //   loop: true,
-  //   autoplay: true,
-  //   autoplayTimeout: 2000,
-  //   autoplayHoverPause: true,
-  //   responsive: {
-  //     0: {
-  //       items: 1,
-  //       nav: false,
-  //     },
-  //     400: {
-  //       items: 2,
-  //       nav: false,
-  //     },
-  //     600: {
-  //       items: 3,
-  //       nav: false,
-  //     },
-  //     1000: {
-  //       items: 5,
-  //       nav: false,
-  //     },
-  //   },
-  // });
-
   $(".owl-carousel").owlCarousel({
     margin: 20,
     center: true,
@@ -99,28 +72,6 @@ $(document).ready(function () {
       },
     },
   });
-  // $(".carouselTestimoni").owlCarousel({
-  //   margin: 20,
-  //   center: true,
-  //   loop: true,
-  //   autoplay: true,
-  //   autoplayTimeOut: 2000,
-  //   autoplayHoverPause: true,
-  //   responsive: {
-  //     0: {
-  //       items: 1,
-  //       nav: false,
-  //     },
-  //     600: {
-  //       items: 1,
-  //       nav: false,
-  //     },
-  //     1000: {
-  //       items: 3,
-  //       nav: false,
-  //     },
-  //   },
-  // });
 });
 
 var nav = document.querySelector("#navbar");
